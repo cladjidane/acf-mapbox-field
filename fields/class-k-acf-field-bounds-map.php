4f6577629cf53729e6e473a44def445ef54d194c
Canu Fabien <fabien.canu@gmail.com>
2022-08-22 11:35:13 +0200

    Update class-k-acf-field-bounds-map.php

--- fields/class-k-acf-field-bounds-map.php
+++ fields/class-k-acf-field-bounds-map.php
@@ -151,7 +151,7 @@ if (!class_exists('K_acf_field_bounds_map')) :
 		*/
 
 ?>
-			<div class="root"></div>
+			<div id="root"></div>
 			<input type="text" name="<?php echo esc_attr($field['name']) ?>" value="<?php echo esc_attr($field['value']) ?>" style="font-size:<?php echo $field['font_size'] ?>px;" />
 <?php
 		}
