bb91dbb570e66e46cae0372ccf417f34723c3dd8
Canu Fabien <fabien.canu@gmail.com>
2022-08-22 10:52:34 +0200

    UP

--- fields/class-k-acf-field-bounds-map.php
+++ fields/class-k-acf-field-bounds-map.php
@@ -1,17 +1,18 @@
 <?php
 
 // exit if accessed directly
-if( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 
 // check if class already exists
-if( !class_exists('K_acf_field_bounds_map') ) :
+if (!class_exists('K_acf_field_bounds_map')) :
 
 
-class K_acf_field_bounds_map extends acf_field {
-	
-	
-	/*
+	class K_acf_field_bounds_map extends acf_field
+	{
+
+
+		/*
 	*  __construct
 	*
 	*  This function will setup the field type data
@@ -23,63 +24,63 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @param	n/a
 	*  @return	n/a
 	*/
-	
-	function __construct( $settings ) {
-		
-		/*
+
+		function __construct($settings)
+		{
+
+			/*
 		*  name (string) Single word, no spaces. Underscores allowed
 		*/
-		
-		$this->name = 'bounds_map';
-		
-		
-		/*
+
+			$this->name = 'bounds_map';
+
+
+			/*
 		*  label (string) Multiple words, can include spaces, visible when selecting a field type
 		*/
-		
-		$this->label = __('FIELD_LABEL', 'TEXTDOMAIN');
-		
-		
-		/*
+
+			$this->label = __('FIELD_LABEL', 'TEXTDOMAIN');
+
+
+			/*
 		*  category (string) basic | content | choice | relational | jquery | layout | CUSTOM GROUP NAME
 		*/
-		
-		$this->category = 'basic';
-		
-		
-		/*
+
+			$this->category = 'basic';
+
+
+			/*
 		*  defaults (array) Array of default settings which are merged into the field object. These are used later in settings
 		*/
-		
-		$this->defaults = array(
-			'font_size'	=> 14,
-		);
-		
-		
-		/*
+
+			$this->defaults = array(
+				'font_size'	=> 14,
+			);
+
+
+			/*
 		*  l10n (array) Array of strings that are used in JavaScript. This allows JS strings to be translated in PHP and loaded via:
 		*  var message = acf._e('bounds_map', 'error');
 		*/
-		
-		$this->l10n = array(
-			'error'	=> __('Error! Please enter a higher value', 'TEXTDOMAIN'),
-		);
-		
-		
-		/*
+
+			$this->l10n = array(
+				'error'	=> __('Error! Please enter a higher value', 'TEXTDOMAIN'),
+			);
+
+
+			/*
 		*  settings (array) Store plugin settings (url, path, version) as a reference for later use with assets
 		*/
-		
-		$this->settings = $settings;
-		
-		
-		// do not delete!
-    	parent::__construct();
-    	
-	}
-	
-	
-	/*
+
+			$this->settings = $settings;
+
+
+			// do not delete!
+			parent::__construct();
+		}
+
+
+		/*
 	*  render_field_settings()
 	*
 	*  Create extra settings for your field. These are visible when editing a field
@@ -91,10 +92,11 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @param	$field (array) the $field being edited
 	*  @return	n/a
 	*/
-	
-	function render_field_settings( $field ) {
-		
-		/*
+
+		function render_field_settings($field)
+		{
+
+			/*
 		*  acf_render_field_setting
 		*
 		*  This function will create a setting for your field. Simply pass the $field parameter and an array of field settings.
@@ -103,20 +105,19 @@ class K_acf_field_bounds_map extends acf_field {
 		*  More than one setting can be added by copy/paste the above code.
 		*  Please note that you must also have a matching $defaults value for the field name (font_size)
 		*/
-		
-		acf_render_field_setting( $field, array(
-			'label'			=> __('Font Size','TEXTDOMAIN'),
-			'instructions'	=> __('Customise the input font size','TEXTDOMAIN'),
-			'type'			=> 'number',
-			'name'			=> 'font_size',
-			'prepend'		=> 'px',
-		));
 
-	}
-	
-	
-	
-	/*
+			acf_render_field_setting($field, array(
+				'label'			=> __('Font Size', 'TEXTDOMAIN'),
+				'instructions'	=> __('Customise the input font size', 'TEXTDOMAIN'),
+				'type'			=> 'number',
+				'name'			=> 'font_size',
+				'prepend'		=> 'px',
+			));
+		}
+
+
+
+		/*
 	*  render_field()
 	*
 	*  Create the HTML interface for your field
@@ -130,31 +131,33 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @param	$field (array) the $field being edited
 	*  @return	n/a
 	*/
-	
-	function render_field( $field ) {
-		
-		
-		/*
+
+		function render_field($field)
+		{
+
+
+			/*
 		*  Review the data of $field.
 		*  This will show what data is available
 		*/
-		
-		echo '<pre>';
-			print_r( $field );
-		echo '</pre>';
-		
-		
-		/*
+
+			echo '<pre>';
+			print_r($field);
+			echo '</pre>';
+
+
+			/*
 		*  Create a simple text input using the 'font_size' setting.
 		*/
-		
-		?>
-		<input type="text" name="<?php echo esc_attr($field['name']) ?>" value="<?php echo esc_attr($field['value']) ?>" style="font-size:<?php echo $field['font_size'] ?>px;" />
-		<?php
-	}
-	
-		
-	/*
+
+?>
+			<div class="root"></div>
+			<input type="text" name="<?php echo esc_attr($field['name']) ?>" value="<?php echo esc_attr($field['value']) ?>" style="font-size:<?php echo $field['font_size'] ?>px;" />
+<?php
+		}
+
+
+		/*
 	*  input_admin_enqueue_scripts()
 	*
 	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
@@ -168,30 +171,26 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @return	n/a
 	*/
 
-	/*
-	
-	function input_admin_enqueue_scripts() {
-		
-		// vars
-		$url = $this->settings['url'];
-		$version = $this->settings['version'];
-		
-		
-		// register & include JS
-		wp_register_script('TEXTDOMAIN', "{$url}assets/js/input.js", array('acf-input'), $version);
-		wp_enqueue_script('TEXTDOMAIN');
-		
-		
-		// register & include CSS
-		wp_register_style('TEXTDOMAIN', "{$url}assets/css/input.css", array('acf-input'), $version);
-		wp_enqueue_style('TEXTDOMAIN');
-		
-	}
-	
-	*/
-	
-	
-	/*
+		function input_admin_enqueue_scripts()
+		{
+
+			// vars
+			$url = $this->settings['url'];
+			$version = $this->settings['version'];
+
+
+			// register & include JS
+			wp_register_script('TEXTDOMAIN', "{$url}assets/index.js", array('acf-input'), $version);
+			wp_enqueue_script('TEXTDOMAIN');
+
+
+			// register & include CSS
+			wp_register_style('TEXTDOMAIN', "{$url}assets/index.css", array('acf-input'), $version);
+			wp_enqueue_style('TEXTDOMAIN');
+		}
+
+
+		/*
 	*  input_admin_head()
 	*
 	*  This action is called in the admin_head action on the edit screen where your field is created.
@@ -205,22 +204,22 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @return	n/a
 	*/
 
-	/*
-		
+		/*
+
 	function input_admin_head() {
-	
-		
-		
+
+
+
 	}
-	
+
 	*/
-	
-	
-	/*
+
+
+		/*
    	*  input_form_data()
    	*
    	*  This function is called once on the 'input' page between the head and footer
-   	*  There are 2 situations where ACF did not load during the 'acf/input_admin_enqueue_scripts' and 
+   	*  There are 2 situations where ACF did not load during the 'acf/input_admin_enqueue_scripts' and
    	*  'acf/input_admin_head' actions because ACF did not know it was going to be used. These situations are
    	*  seen on comments / user edit forms on the front end. This function will always be called, and includes
    	*  $args that related to the current screen such as $args['post_id']
@@ -232,19 +231,19 @@ class K_acf_field_bounds_map extends acf_field {
    	*  @param	$args (array)
    	*  @return	n/a
    	*/
-   	
-   	/*
-   	
+
+		/*
+
    	function input_form_data( $args ) {
-	   	
-		
-	
+
+
+
    	}
-   	
+
    	*/
-	
-	
-	/*
+
+
+		/*
 	*  input_admin_footer()
 	*
 	*  This action is called in the admin_footer action on the edit screen where your field is created.
@@ -258,18 +257,18 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @return	n/a
 	*/
 
-	/*
-		
+		/*
+
 	function input_admin_footer() {
-	
-		
-		
+
+
+
 	}
-	
+
 	*/
-	
-	
-	/*
+
+
+		/*
 	*  field_group_admin_enqueue_scripts()
 	*
 	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
@@ -283,16 +282,16 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @return	n/a
 	*/
 
-	/*
-	
+		/*
+
 	function field_group_admin_enqueue_scripts() {
-		
+
 	}
-	
+
 	*/
 
-	
-	/*
+
+		/*
 	*  field_group_admin_head()
 	*
 	*  This action is called in the admin_head action on the edit screen where your field is edited.
@@ -306,16 +305,16 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @return	n/a
 	*/
 
-	/*
-	
+		/*
+
 	function field_group_admin_head() {
-	
+
 	}
-	
+
 	*/
 
 
-	/*
+		/*
 	*  load_value()
 	*
 	*  This filter is applied to the $value after it is loaded from the db
@@ -329,19 +328,19 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$value
 	*/
-	
-	/*
-	
+
+		/*
+
 	function load_value( $value, $post_id, $field ) {
-		
+
 		return $value;
-		
+
 	}
-	
+
 	*/
-	
-	
-	/*
+
+
+		/*
 	*  update_value()
 	*
 	*  This filter is applied to the $value before it is saved in the db
@@ -355,19 +354,19 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$value
 	*/
-	
-	/*
-	
+
+		/*
+
 	function update_value( $value, $post_id, $field ) {
-		
+
 		return $value;
-		
+
 	}
-	
+
 	*/
-	
-	
-	/*
+
+
+		/*
 	*  format_value()
 	*
 	*  This filter is appied to the $value after it is loaded from the db and before it is returned to the template
@@ -382,36 +381,36 @@ class K_acf_field_bounds_map extends acf_field {
 	*
 	*  @return	$value (mixed) the modified value
 	*/
-		
-	/*
-	
+
+		/*
+
 	function format_value( $value, $post_id, $field ) {
-		
+
 		// bail early if no value
 		if( empty($value) ) {
-		
+
 			return $value;
-			
+
 		}
-		
-		
+
+
 		// apply setting
-		if( $field['font_size'] > 12 ) { 
-			
+		if( $field['font_size'] > 12 ) {
+
 			// format the value
 			// $value = 'something';
-		
+
 		}
-		
-		
+
+
 		// return
 		return $value;
 	}
-	
+
 	*/
-	
-	
-	/*
+
+
+		/*
 	*  validate_value()
 	*
 	*  This filter is used to perform validation on the value prior to saving.
@@ -428,34 +427,34 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @param	$input (string) the corresponding input name for $_POST value
 	*  @return	$valid
 	*/
-	
-	/*
-	
+
+		/*
+
 	function validate_value( $valid, $value, $field, $input ){
-		
+
 		// Basic usage
 		if( $value < $field['custom_minimum_setting'] )
 		{
 			$valid = false;
 		}
-		
-		
+
+
 		// Advanced usage
 		if( $value < $field['custom_minimum_setting'] )
 		{
 			$valid = __('The value is too little!','TEXTDOMAIN'),
 		}
-		
-		
+
+
 		// return
 		return $valid;
-		
+
 	}
-	
+
 	*/
-	
-	
-	/*
+
+
+		/*
 	*  delete_value()
 	*
 	*  This action is fired after a value has been deleted from the db.
@@ -469,43 +468,43 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @param	$key (string) the $meta_key which the value was deleted
 	*  @return	n/a
 	*/
-	
-	/*
-	
+
+		/*
+
 	function delete_value( $post_id, $key ) {
-		
-		
-		
+
+
+
 	}
-	
+
 	*/
-	
-	
-	/*
+
+
+		/*
 	*  load_field()
 	*
 	*  This filter is applied to the $field after it is loaded from the database
 	*
 	*  @type	filter
 	*  @date	23/01/2013
-	*  @since	3.6.0	
+	*  @since	3.6.0
 	*
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$field
 	*/
-	
-	/*
-	
+
+		/*
+
 	function load_field( $field ) {
-		
+
 		return $field;
-		
-	}	
-	
+
+	}
+
 	*/
-	
-	
-	/*
+
+
+		/*
 	*  update_field()
 	*
 	*  This filter is applied to the $field before it is saved to the database
@@ -517,19 +516,19 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$field
 	*/
-	
-	/*
-	
+
+		/*
+
 	function update_field( $field ) {
-		
+
 		return $field;
-		
-	}	
-	
+
+	}
+
 	*/
-	
-	
-	/*
+
+
+		/*
 	*  delete_field()
 	*
 	*  This action is fired after a field is deleted from the database
@@ -541,23 +540,21 @@ class K_acf_field_bounds_map extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	n/a
 	*/
-	
-	/*
-	
+
+		/*
+
 	function delete_field( $field ) {
-		
-		
-		
-	}	
-	
+
+
+
+	}
+
 	*/
-	
-	
-}
+	}
 
 
-// initialize
-new K_acf_field_bounds_map( $this->settings );
+	// initialize
+	new K_acf_field_bounds_map($this->settings);
 
 
 // class_exists check
